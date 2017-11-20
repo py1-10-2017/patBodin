8d1c703ef2f398ab3411a6fea51a0b60fc6ee28a
pm0n3s <patrick.bodin777@gmail.com>
2017-11-19 21:21:33 -0500

    working file

---
+++ DjangoORM/Books_Authors/apps/book_authors/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Authour(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    books = models.ManyToManyField(Book, related_name="authors")
